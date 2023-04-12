c36c03b464998967f2dbda6874689fd8364392b8
Bruno Vinícius <vbruno96@gmail.com>
2023-04-12 13:33:20 -0300

    transformation example

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,25 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const createUserFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty('Nome obrigatorio')
+    .transform((name) => {
+      return name
+        .trim()
+        .split(' ')
+        .map((word) => word[0].toLocaleUpperCase().concat(word.substring(1)))
+        .join(' ')
+    }),
   email: z
     .string()
     .nonempty('Email obrigatorio')
-    .email('Formato de email ivalido'),
+    .email('Formato de email ivalido')
+    .refine(
+      (email) => email.endsWith('@gmail.com'),
+      'Precisa ser um email gmail',
+    ),
+
   password: z.string().min(6, 'A senha precisa de no minimo 6 caracteres'),
 })
 
@@ -35,6 +50,16 @@ export function App() {
         onSubmit={handleSubmit(createUser)}
         className="flex flex-col gap-4 w-full max-w-xs"
       >
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">Nome</label>
+          <input
+            className="border bg-zinc-800 border-slate-500 text-zinc-100 shadow-sm rounded h-10 px-3"
+            type="text"
+            {...register('name')}
+          />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+
         <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
           <input
